bf721edde4ac0186e4ac2f6d887e26ad3d07927e
Aaron Yordanyan <aaron.yor@gmail.com>
2023-05-08 10:05:23 +0400

    Rename base producer options

--- producer_opts.go
+++ producer_opts.go
@@ -2,14 +2,14 @@ package easykafka
 
 import "github.com/segmentio/kafka-go"
 
-func WithWriterConfig[T any](writer *kafka.Writer) BaseProducerOption {
+func BaseProducerWithWriterConfig(writer *kafka.Writer) BaseProducerOption {
 	return func(k *BaseProducer) (err error) {
 		k.writer = writer
 		return nil
 	}
 }
 
-func ProducerInitialPartitionsCount[T any](count uint) BaseProducerOption {
+func BaseProducerInitialPartitionsCount(count uint) BaseProducerOption {
 	return func(c *BaseProducer) (err error) {
 		c.partitions = count
 		return nil
